ed5e0acb9c2394deec3c539f55107f18229280a5
slackandhack <charles.austin.bradley@gmail.com>
2013-11-30 19:44:11 -0500

    Initialized test project for android client

---
+++ AwesomeClientTest/src/com/example/awesome_toe/test/MainActivityTest.java
@@ -0,0 +1,12 @@
+package com.example.awesome_toe.test;
+
+import android.test.ActivityUnitTestCase;
+
+public class MainActivityTest extends ActivityUnitTestCase {
+
+	public MainActivityTest(Class activityClass) {
+		super(activityClass);
+		// TODO Auto-generated constructor stub
+	}
+
+}
