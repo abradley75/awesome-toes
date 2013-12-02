8799f8f909b9976ab25a9d01b9b0677cbb907959
slackandhack <charles.austin.bradley@gmail.com>
2013-12-01 23:11:37 -0500

    Updated the UpdatePacket

--- androidClient/src/com/example/awesome_toe/UpdatePacket.java
+++ androidClient/src/com/example/awesome_toe/UpdatePacket.java
@@ -1,23 +1,42 @@
 package com.example.awesome_toe;
-import java.util.Date;
 
 public class UpdatePacket {
-    private final int value;
     
-    public UpdatePacket() {
-        this((int) (System.currentTimeMillis()));
-    }
+	private final char playerTurn;
+	private final char[][] boardState; 
     
-    public UpdatePacket(int value) {
-        this.value = value;
+    public UpdatePacket(char in_playerTurn, char[][] in_boardState) {
+        this.playerTurn = in_playerTurn;
+        this.boardState = in_boardState;
     }
         
-    public int value() {
-        return value;
+    public char getPlayerTurn() {
+        return playerTurn;
+    }
+    
+    public char[][] getBoardState() {
+    	return boardState;
     }
         
+    // Prints board contents row order first.
     @Override
     public String toString() {
-        return new Date(value()+25L*24L*60L*60L*1000L).toString();
+        String str1 = "**Update Packet** CurrentTurn: " + playerTurn;
+        String str2 = ", Board Contents: [";
+        String str4 = "]";
+        
+        String str3 = "";
+        for (int j = 0 ; j < 5 ; j++) {
+        	for (int i = 0 ; i < 5 ; i++) {
+        		str3 = str3 + boardState[i][j] ;
+        		
+        		if(i == 5)
+        			str3 = str3 + ", ";
+        		else
+        			str3 = str3 + " ";
+        	}
+        }
+        
+        return str1 + str2 + str3 + str4 ;        
     }
 }
