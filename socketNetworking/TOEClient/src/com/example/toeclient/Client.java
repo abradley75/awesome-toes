426ed05c23d182735bf678535108dd15da76e2e4
Chang Zheng <chang@chang-MT3707.(none)>
2013-12-14 19:15:47 -0500

    fixed set board button

--- socketNetworking/TOEClient/src/com/example/toeclient/Client.java
+++ socketNetworking/TOEClient/src/com/example/toeclient/Client.java
@@ -31,7 +31,11 @@ public class Client implements Runnable{
 			parseInitialMessage(message);
 			while(true){			
 				message = (String)m_iStream.readObject();
-				//game.setMessage(message);			
+				if(message != null)
+					parseMessage(message);
+				//game.setMessage(message);	
+				message = null;
+				Thread.sleep(500);//sleep for 500
 			}		
 		} catch (OptionalDataException e) {
 			// TODO Auto-generated catch block
@@ -45,17 +49,25 @@ public class Client implements Runnable{
 		}
 	}//end of run
 
+	private void parseMessage(String message) {
+		// TODO Auto-generated method stub
+		String[] splitMsg = message.split("\\r?\\n");
+		int playerCt = Integer.parseInt(splitMsg[0].split(":")[1]);
+		String turn = splitMsg[1].split(":")[1];
+	}
+
 	private void parseInitialMessage(String message) throws IOException {
 		// TODO Auto-generated method stub
 		System.out.println(message);
 		String[] splitMsg = message.split("\\r?\\n");
-		int gameState = Integer.parseInt(splitMsg[0].split(":")[1]);
-		System.out.println(gameState);
+		int player = Integer.parseInt(splitMsg[0].split(":")[1]);
+		System.out.println(player);
 		String piece = splitMsg[1].split(":")[1];
 		System.out.println(piece);
-		if(gameState == -1 && piece == "t"){
-			game.enableButtons();
+		
+		if(player==1 && piece.equals("t")){
 			game.setMessage("SET THE BOARD SIZE");
+			game.enableButtons();			
 		}
 		
 	}
@@ -64,5 +76,7 @@ public class Client implements Runnable{
 		m_oStream.reset();
 		m_oStream.writeObject(message);
 	}
+	
+	
 
 }
